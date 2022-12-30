4ad30dd9ff7477918a9bc4149c2b8cbe0d059e45
Tabaze <83618505+Tabaze@users.noreply.github.com>
2022-12-30 22:09:37 +0100

    add
    fix problems and add nex monh days

--- all.js
+++ all.js
@@ -28,12 +28,12 @@ const months = [
 
 function initCalendar() {
     //to get all prev month days and currnets month all days and rem next month days
-    const firstDay = new Date(year, month + 1)
+    const firstDay = new Date(year, month , 1)
     const lastDay = new Date(year, month + 1, 0)
     const prevLastDay = new Date(year, month, 0)
     const prevDays = prevLastDay.getDate()
     const lastDate = lastDay.getDate()
-    const day = firstDay.getDate()
+    const day = firstDay.getDay()
     const nextDays = 7 - lastDay.getDay() - 1
 
     //update date in the top of calendar
@@ -42,12 +42,13 @@ function initCalendar() {
     // Adding Days on dom
     let days = ""
     //prev month days
+    console.log(firstDay)
     for (let x = day; x > 0; x--) {
-        days += `<div class="day pre-date">${prevDays - x + 1}</div>`
+        console.log(x)
+        days += `<div class="day prev-date">${prevDays - x + 1}</div>`
     }
     //current month
-    console.log(lastDate)
-    for (let x = 1; x < lastDate; x++) {
+    for (let x = 1; x <= lastDate; x++) {
         //if day is today add class today
         if (x == new Date().getDate() && year == new Date().getFullYear() && month == new Date().getMonth()) {
             days += `<div class="day today">${x}</div>`
@@ -56,6 +57,10 @@ function initCalendar() {
         else {
             days += `<div class="day">${x}</div>`
         }
+    }
+    //next month days
+    for (let x = 1; x < nextDays; x++) {
+        days += `<div class="day next-date">${x}</div>`
     }
     daysContainer.html(days)
 }
